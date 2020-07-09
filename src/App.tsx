799b4b8d994609d00ff3163e6b0833508e191eaf
Takamichi Omori <merrytakamerry@gmail.com>
2020-07-10 00:57:26 +0900

    :poop: implement bet, fold, check

--- src/App.tsx
+++ src/App.tsx
@@ -27,29 +27,51 @@ const style3 = {
     padding: '30px',
 };
 
-type Flag = 'A' | 'B';
-
-const ListItemLink = (props: any) =>  {
-  return <ListItem button component="a" {...props} />;
-}
 
 function App() {
-  const [flag, setFlag] = useState<Flag>('A');
+  const [flag, setFlag] = useState<boolean>(false);
   const [pool, setPool] = useState<number>(0);
-  const APoint = 50000;
-  const BPoint = 50000;
+  const [betValue, setBetValue] = useState<number>(0);
+  const [APoint, setAPoint] = useState<number>(50000);
+  const [BPoint, setBPoint] = useState<number>(50000);
+
+  const bet = () => {
+    const User = !flag? 'A': 'B'
+    alert(User + " bet " + betValue);
+    if(User === 'A'){
+      setAPoint(APoint - betValue);
+    }else{
+      setBPoint(BPoint - betValue);
+    }
+    setPool(pool + betValue);
+    setBetValue(0);
+    setFlag(!flag);
+    setBetValue(0);
+  }
   
+  const fold = () => {
+    const User = flag? 'A': 'B';
+    alert(`Congratsration! \n${User} wins ${pool}!`);
+    if(User === 'A'){
+      setAPoint(APoint + pool);
+    }else{
+      setBPoint(BPoint + pool);
+    }
+    setPool(0);
+    setBetValue(0);
+  }
+
   return (
     <div className="App">
       <Grid container alignItems="center" justify="center">
-        <Grid item xs={4}　style={(flag==='A')? style3 :style1} onClick={() => setFlag('A')}>
+        <Grid item xs={4}　style={flag===false? style3 :style1} onClick={() => setFlag(!flag)}>
           {APoint}
         </Grid>
         <Grid item xs={4} style={style2}>
           <Container >
             <List component="nav" aria-label="secondary mailbox folders" >
               <ListItem>
-                <ListItemText primary={"Now:  " + flag} />
+                <ListItemText primary={"Now:  " + (!flag?'A':'B')} />
               </ListItem>
               <ListItem>
                 <ListItemText primary={"Pool: " + pool} />
@@ -57,30 +79,30 @@ function App() {
             </List>
             </Container>  
         </Grid>
-        <Grid item xs={4} style={(flag==='B')? style3 :style1} onClick={() => setFlag('B')}>
+        <Grid item xs={4} style={flag? style3 :style1} onClick={() => setFlag(!flag)}>
           {BPoint}
         </Grid>
       </Grid>
       <br></br>
       <Grid container alignItems="center" justify="center" >
         <Grid item xs={2}>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={() => setFlag(!flag)}>
             Check
           </Button>
         </Grid>
         <Grid item xs={2}>
-          <Button variant="contained" color="secondary">
+          <Button variant="contained" color="secondary" onClick={() => bet()}>
             Bet
           </Button>
         </Grid>        
         <Grid item xs={2}>
-          <Button variant="contained" color="inherit">
+          <Button variant="contained" color="inherit" onClick={() => fold()}>
             Fold
           </Button>
         </Grid>
       </Grid>
       <br/>
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={e => setBetValue(parseInt(e.target.value))}/>
 
     </div>
   );
